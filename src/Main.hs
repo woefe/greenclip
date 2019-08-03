73ec3232251b32611cfc86c64a567e7ab5bc5b10
Wolfgang Popp <mail@wolfgang-popp.de>
2019-08-03 13:12:55 +0200

    Improve path handling
    Also fixes edge cases when directories are missing

--- src/Main.hs
+++ src/Main.hs
@@ -9,19 +9,19 @@
 
 module Main where
 
-import           Protolude             hiding (readFile, to, (<&>), (&))
+import           Protolude             hiding (readFile, to, (<&>), (&), (<.>))
 
 import           Control.Monad.Catch   (MonadCatch, catchAll)
 import           Data.Binary           (decode, encode)
 import qualified Data.ByteString       as B
 import           Data.Hashable         (hash)
-import           Data.List             (dropWhileEnd)
 import qualified Data.Text             as T
 import           Data.Vector           (Vector)
 import qualified Data.Vector           as V
 import           Lens.Micro
 import           Lens.Micro.Mtl
 import qualified System.Directory      as Dir
+import           System.FilePath       (takeDirectory, (</>), (<.>))
 import           System.Posix.Files    (setFileMode)
 import           System.Environment    (lookupEnv)
 import           System.IO             (IOMode (..), hClose, hGetContents,
@@ -36,9 +36,9 @@ data Command = DAEMON | PRINT | COPY Text | CLEAR | HELP deriving (Show, Read)
 
 data Config = Config
   { maxHistoryLength           :: Int
-  , historyPath                :: Text
-  , staticHistoryPath          :: Text
-  , imageCachePath             :: Text
+  , historyPath                :: FilePath
+  , staticHistoryPath          :: FilePath
+  , imageCachePath             :: FilePath
   , usePrimarySelectionAsInput :: Bool
   , blacklistedApps            :: [Text]
   , trimSpaceFromSelection     :: Bool
@@ -54,7 +54,7 @@ readFile filepath = bracket (openFile filepath ReadMode) hClose $ \h -> do
 
 getHistory :: (MonadIO m, MonadReader Config m) => m ClipHistory
 getHistory = do
-  storePath <- view $ to (toS . historyPath)
+  storePath <- view $ to historyPath
   liftIO $ readH storePath `catchAll` const mempty
   where
     readH filePath = B.readFile filePath <&> V.fromList . decode . toS
@@ -62,7 +62,7 @@ getHistory = do
 
 getStaticHistory :: (MonadIO m, MonadReader Config m) => m ClipHistory
 getStaticHistory = do
-  storePath <- view $ to (toS . staticHistoryPath)
+  storePath <- view $ to staticHistoryPath
   liftIO $ readH storePath `catchAll` const mempty
   where
     readH filePath = readFile filePath <&> V.fromList . fmap toSelection . T.lines . toS
@@ -71,7 +71,7 @@ getStaticHistory = do
 
 storeHistory :: (MonadIO m, MonadReader Config m) => ClipHistory -> m ()
 storeHistory history = do
-  storePath <- view $ to (toS . historyPath)
+  storePath <- view $ to historyPath
   liftIO $ writeH storePath history
   where
     writeH storePath = B.writeFile storePath . toS . encode . V.toList
@@ -82,16 +82,16 @@ appendToHistory sel history' = do
   trimSelection <- view $ to trimSpaceFromSelection
   case sel of
     Clip.Selection appName (Clip.UTF8 txt) -> appendGeneric (if trimSelection then Clip.Selection appName (Clip.UTF8 (T.strip txt)) else sel) history'
-    Clip.Selection _ (Clip.PNG bytes) -> appendImage Clip.PNG ".png" bytes
-    Clip.Selection _ (Clip.JPEG bytes) -> appendImage Clip.JPEG ".jpeg" bytes
-    Clip.Selection _ (Clip.BITMAP bytes) -> appendImage Clip.BITMAP ".bmp" bytes
+    Clip.Selection _ (Clip.PNG bytes) -> appendImage Clip.PNG "png" bytes
+    Clip.Selection _ (Clip.JPEG bytes) -> appendImage Clip.JPEG "jpeg" bytes
+    Clip.Selection _ (Clip.BITMAP bytes) -> appendImage Clip.BITMAP "bmp" bytes
 
 
   where
     appendImage imgCtr extension bytes = do
       cachePth <- view (to imageCachePath)
       let imgHash = show $ hash bytes
-      let imgPath = toS $ cachePth <> "/" <> imgHash <> extension
+      let imgPath = cachePth </> imgHash <.> extension
       _ <- liftIO $ writeImage imgPath bytes
       appendGeneric (sel {Clip.selection = imgCtr $ toS imgHash}) history'
 
@@ -111,20 +111,13 @@ appendToHistory sel history' = do
 
 setHistoryFilePermission :: (MonadIO m, MonadReader Config m) => m ()
 setHistoryFilePermission = do
-  storePath <- view $ to (toS . historyPath)
+  storePath <- view $ to historyPath
   fileExist <- liftIO $ Dir.doesFileExist storePath
   when (not fileExist) (storeHistory mempty)
   liftIO $ setFileMode storePath 0o600
 
-prepareDirs :: (MonadIO m, MonadReader Config m) => m ()
-prepareDirs = do
-  historyFile <- view $ to (T.unpack . historyPath)
-  imgDir <- view $ to (T.unpack . imageCachePath)
-  let dirs = [imgDir, dropWhileEnd (/= '/') historyFile]
-  mapM_ (liftIO . Dir.createDirectoryIfMissing True) dirs
-
 runDaemon:: (MonadIO m, MonadCatch m, MonadReader Config m) => m ()
-runDaemon = prepareDirs >> setHistoryFilePermission >> (forever $ go `catchAll` handleError)
+runDaemon = setHistoryFilePermission >> (forever $ go `catchAll` handleError)
   where
     _0_5sec :: Int
     _0_5sec = 5 * 100000
@@ -175,14 +168,14 @@ runDaemon = prepareDirs >> setHistoryFilePermission >> (forever $ go `catchAll` 
     getSelectionFrom :: IO (Maybe Clip.Selection) -> IO (Maybe Clip.Selection)
     getSelectionFrom = fmap join . timeout _5sec
 
-    purgeSelection (Clip.Selection _ (Clip.PNG txt)) = purge (toS txt <> ".png")
-    purgeSelection (Clip.Selection _ (Clip.JPEG txt)) = purge (toS txt <> ".jpeg")
-    purgeSelection (Clip.Selection _ (Clip.BITMAP txt)) = purge (toS txt <> ".bmp")
+    purgeSelection (Clip.Selection _ (Clip.PNG txt)) = purge (toS txt <.> "png")
+    purgeSelection (Clip.Selection _ (Clip.JPEG txt)) = purge (toS txt <.> "jpeg")
+    purgeSelection (Clip.Selection _ (Clip.BITMAP txt)) = purge (toS txt <.> "bmp")
     purgeSelection _ = return ()
 
     purge path = do
       cachePth <- view (to imageCachePath)
-      liftIO $ Dir.removeFile (toS $ cachePth <> "/" <> path) `catchAll` const mempty
+      liftIO $ Dir.removeFile (cachePth </> path) `catchAll` const mempty
 
     handleError ex = do
       let displayMissing = "openDisplay" `T.isInfixOf` show ex
@@ -198,14 +191,14 @@ toRofiStr (Clip.Selection appName (Clip.PNG txt)) = "image/png " <> appName <> "
 toRofiStr (Clip.Selection appName (Clip.JPEG txt)) = "image/jpeg " <> appName <> " " <> toS txt
 toRofiStr (Clip.Selection appName (Clip.BITMAP txt)) = "image/bmp " <> appName <> " " <> toS txt
 
-fromRofiStr :: Text -> Text -> IO Clip.Selection
-fromRofiStr cachePth txt@(T.isPrefixOf "image/png " -> True) = B.readFile (toS $ cachePth <> "/" <> getHash txt <> ".png") <&> Clip.Selection "greenclip" . Clip.PNG
-fromRofiStr cachePth txt@(T.isPrefixOf "image/jpeg " -> True) = B.readFile (toS $ cachePth <> "/" <> getHash txt <> ".jpeg") <&> Clip.Selection "greenclip" . Clip.JPEG
-fromRofiStr cachePth txt@(T.isPrefixOf "image/bmp " -> True) = B.readFile (toS $ cachePth <> "/" <> getHash txt <> ".bmp") <&> Clip.Selection "greenclip" . Clip.BITMAP
+fromRofiStr :: FilePath -> Text -> IO Clip.Selection
+fromRofiStr cachePth txt@(T.isPrefixOf "image/png " -> True)  = B.readFile (cachePth </> getHash txt <.> "png")  <&> Clip.Selection "greenclip" . Clip.PNG
+fromRofiStr cachePth txt@(T.isPrefixOf "image/jpeg " -> True) = B.readFile (cachePth </> getHash txt <.> "jpeg") <&> Clip.Selection "greenclip" . Clip.JPEG
+fromRofiStr cachePth txt@(T.isPrefixOf "image/bmp " -> True)  = B.readFile (cachePth </> getHash txt <.> "bmp")  <&> Clip.Selection "greenclip" . Clip.BITMAP
 fromRofiStr _ txt = return $ Clip.Selection "greenclip" (Clip.UTF8 (T.map (\c -> if c == '\xA0' then '\n' else c) txt))
 
-getHash :: Text -> Text
-getHash = fromMaybe mempty . lastMay . T.split (== ' ')
+getHash :: Text -> FilePath
+getHash = toS . fromMaybe mempty . lastMay . T.split (== ' ')
 
 
 printHistoryForRofi :: (MonadIO m, MonadReader Config m) => m ()
@@ -222,11 +215,8 @@ advertiseSelection txt = do
   liftIO $ Clip.setClipboardSelection selection
 
 
-getConfig :: IO Config
-getConfig = do
-  home <- Dir.getHomeDirectory
-  let cfgPath = home <> "/.config/greenclip.cfg"
-
+getConfig :: FilePath -> IO Config
+getConfig cfgPath = do
   cfgStr <- readFile cfgPath `catchAll` const mempty
 
   let unprettyCfg' = cfgStr & T.strip . T.replace "\n" "" . toS
@@ -235,12 +225,6 @@ getConfig = do
                       else T.replace "}" ", trimSpaceFromSelection = True }" unprettyCfg'
   let cfgMaybe = readMaybe $ toS unprettyCfg
   let cfg = fromMaybe defaultConfig cfgMaybe
-
-  -- Write back the config file if the current one was invalid
-  _ <- if isNothing cfgMaybe || unprettyCfg /= unprettyCfg'
-        then let prettyCfg = show cfg & T.replace "," ",\n" . T.replace "{" "{\n " . T.replace "}" "\n}"
-             in writeFile cfgPath prettyCfg
-        else return ()
 
   historyPath' <- expandHomeDir $ cfg ^. to historyPath
   staticHistoryPath' <- expandHomeDir $ cfg ^. to staticHistoryPath
@@ -253,7 +237,25 @@ getConfig = do
 
   where
     defaultConfig = Config 50 "~/.cache/greenclip.history" "~/.cache/greenclip.staticHistory" "/tmp/greenclip/" False [] True
-    expandHomeDir str = (toS . fromMaybe (toS str) . listToMaybe <$> wordexp (toS str)) `catchAll` (\_ -> return $ toS str)
+    expandHomeDir str = (fromMaybe str . listToMaybe <$> wordexp str) `catchAll` (\_ -> return str)
+
+
+prepareDirs :: Config -> IO ()
+prepareDirs cfg = do
+  home <- Dir.getHomeDirectory
+  let dirs = [ takeDirectory $ staticHistoryPath cfg
+             , takeDirectory $ historyPath cfg
+             , imageCachePath cfg
+             , home </> ".config"
+             , home </> ".cache"]
+  mapM_ (Dir.createDirectoryIfMissing True) dirs
+
+
+writeConfig :: FilePath -> Config -> IO ()
+writeConfig cfgPath cfg =
+  let prettyCfg = show cfg & T.replace "," ",\n" . T.replace "{" "{\n " . T.replace "}" "\n}"
+  in writeFile cfgPath prettyCfg
+
 
 
 parseArgs :: [Text] -> Command
@@ -265,7 +267,12 @@ parseArgs _              = HELP
 
 run :: Command -> IO ()
 run cmd = do
-  cfg <- getConfig
+  cfgPath <- (</> ".config/greenclip.cfg") <$> Dir.getHomeDirectory
+
+  cfg <- getConfig cfgPath
+  prepareDirs cfg
+  writeConfig cfgPath cfg
+
   case cmd of
     DAEMON   -> runReaderT runDaemon cfg
     PRINT    -> runReaderT printHistoryForRofi cfg
